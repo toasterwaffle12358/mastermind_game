144b735983424799c3be9deb4bb816cd72609eda
toasterwaffle12358 <115607216+toasterwaffle12358@users.noreply.github.com>
2023-09-26 13:52:27 -0700

    upload

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import kotlin.random.Random
 
 fun main() {
     //variable to keep main game loop running
-    var hasbeensolved = false
+    var whileloopcount: Int = 0
 
     //setting up colors
     // Everything after this is in red
@@ -13,27 +13,40 @@ fun main() {
 
     //creating the solution colors and order as a list, will be used to compare against user inputs to see if user is correct
     val solutionlist = listOf(Random.nextInt(1,7), Random.nextInt(1,7), Random.nextInt(1,7), Random.nextInt(1,7))
-    println("solution is: $solutionlist this line is for testing, remove in final build")
+
 
     println("welcome!")
 
     //main game loop
-    while (hasbeensolved == false) {
+    while (whileloopcount < 10) {
 
         //taking user input
         println("guess 4 numbers from 1 to 6 seperated by only commas. eg: 4,6,5,1")
-        var guessstrings = readln().split(",")
+        println("type \"i give up\" to get the solution")
+
+        //getting user input
+        val guessinput = readln()
+
+        //testing to see if user gives up
+        if (guessinput == "i give up" ) {
+            println("solution is: $solutionlist")
+            break
+        }
+
+        //turning user input into list from string so that it can be compared to the solution list
+        val guessstrings = guessinput.split(",")
 
         //testing user input to make sure its formatted properly and won't break the map function
         // also to make sure that user is only inputting 4 numbers from  1 to 6
         if (guessstrings.count() != 4) {
             println("input syntax wrong, try again.")
-            break
+            continue
         }
         if (guessstrings.filterNot { s -> (s == "1") || (s == "2") || (s == "3") || (s == "4") || (s == "5") || (s == "6") }.isNotEmpty()) {
             println("input syntax wrong, try again.")
-            break
+            continue
         }
+        whileloopcount++
 
         //turning user input into proper formatting (formatted as a list of integers, just like the solution list)
         var guess = guessstrings.map { it.toInt() }
@@ -43,15 +56,26 @@ fun main() {
         var correctcounter = 0
         var correctbutwrongspotcounter = 0
         var onlistitem = 0
+        var solutionlistavailible = solutionlist.toMutableList()
+        var guesslistavailible = guess.toMutableList()
 
+        //calculating numbers correct in right and wrong spot
         for (item in guess) {
-            when {
-                (item == solutionlist[onlistitem]) -> correctcounter++
-                (solutionlist.contains(item) == true) -> correctbutwrongspotcounter++
+            if (item == solutionlist[onlistitem]) {
+                correctcounter++
+                solutionlistavailible[onlistitem] = 10
+                guesslistavailible[onlistitem] = 11
             }
             onlistitem++
         }
+        for (item in guesslistavailible) {
+            if (solutionlistavailible.contains(item) == true) {
+                correctbutwrongspotcounter++
+            }
+        }
 
+
+        //what to do if the user gets all numbers correct
         if (correctcounter == 4) {
             println("wowie holy cow no way you did it so cool omg")
             break
@@ -60,9 +84,11 @@ fun main() {
         //printing results of user input list compared to solution list
         println("you got " + colorpurple +correctcounter + colorreset + " correct number(s) in the right spot!")
         println("you got " + colorpurple +correctbutwrongspotcounter + colorreset + " number(s) correct but in the wrong spot")
-
-        //testing just to make sure that the input was processed properly
-        println("your guess was $guess this line is for testing, remove in final build.")
+        println("you have " + colorpurple + (10 - whileloopcount) + colorreset + " guesses left")
+        if (whileloopcount == 10) {
+            println("you lost, no guesses left :(")
+            println("solution is: $solutionlist")
+        }
 
     }
 
