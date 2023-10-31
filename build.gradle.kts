ae64faebb7459acdb690568c01d9e79cf9bc09f1
toasterwaffle12358 <115607216+toasterwaffle12358@users.noreply.github.com>
2023-10-31 13:25:09 -0700

    added history so user can see past guesses easily

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
 plugins {
     kotlin("jvm") version "1.9.0"
     application
